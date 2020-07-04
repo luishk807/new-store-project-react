c2b9a4593da2aed6e2a139fade07353ed3a58b8b
Luis Ho <evil_luis@hotmail.com>
2020-07-04 13:00:54 -0400

    add search bar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,8 +5,10 @@ import {
   PermIdentityIcon,
   ShoppingCartIcon,
 } from '@material-ui/icons';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import SearchIcon from '@material-ui/icons/Search';
 import MenuIcon from '@material-ui/icons/Menu';
 import { 
   AppBar,
@@ -26,7 +28,8 @@ import {
   List,
   ListItem,
   ListItemIcon,
-  ListItemText
+  ListItemText,
+  InputBase
 } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 
@@ -44,7 +47,44 @@ const styles = (theme) => ({
   },
   sideMenuRoot: {
     width: 250,
-  }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
 })
 
 const Header = ({classes, data}) => {
@@ -84,6 +124,24 @@ const Header = ({classes, data}) => {
             <MenuIcon />
           </IconButton>
           <div className={classes.title}><Link href="/"><img src="/images/logo-white.png" alt="" /></Link></div>
+          <Button href="#" color="inherit">
+            <Typography className={classes.title} variant="h6" noWrap>
+              category
+            </Typography>
+          </Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
           <Button href="#" color="inherit"><ShoppingCartOutlinedIcon style={{ fontSize: 40 }} /></Button>
           <Button href="#" color="inherit"><PermIdentityOutlinedIcon style={{ fontSize: 40 }} /></Button>
         </Toolbar>

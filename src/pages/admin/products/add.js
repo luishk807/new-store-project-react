95de8ecbaef186dad144f0f53e81066948b9d0fc
Luis Ho <evil_luis@hotmail.com>
2020-08-02 15:38:43 -0400

    work on form

--- src/pages/admin/products/add.js
+++ src/pages/admin/products/add.js
@@ -15,7 +15,10 @@ import {
   TextField,
   TextareaAutosize
 } from '@material-ui/core';
-import MuiAlert from '@material-ui/lab/Alert';
+import { 
+  MuiAlert,
+  Autocomplete,
+} from '@material-ui/lab';
 import {DropzoneDialog} from 'material-ui-dropzone'
 import ColorPicker from 'material-ui-color-picker'
 
@@ -57,29 +60,59 @@ const styles = (theme) => ({
 });
 
 const Add = ({classes}) => {
-  const [category, setCategory] = useState(CategorySample[0].id);
-  const [brand, setBrand] = useState(BrandsSample[0].id);
-  const [vendor, setVendor] = useState(VendorSample[0].id);
-  const [formErrors, setFormErrors] = useState({
-    name: false,
-    stock: false,
-    category: false,
-    amount: false,
-    brand: false,
-    model: false,
-    code: false,
-    description: false,
-  })
-  const [openSnack, setOpenSnack] = useState(false);
-  const categories = CategorySample; 
+  const categories = CategorySample;
   const brands = BrandsSample;
   const vendors = VendorSample;
-
-  const [file, setFiles] = useState({
-    open: false,
-    files: []
-  });
+  const [openSnack, setOpenSnack] = useState(false);
+  const [form, setForm] = useState({
+    name: {
+      value: '',
+      error:false,
+    },
+    stock: {
+      value: '',
+      error: false,
+    },
+    category: {
+      value: categories[0].id,
+      error: false,
+    },
+    amount: {
+      value: '',
+      error: false,
+    },
+    brand: {
+      value: brands[0],
+      error: false,
+    },
+    model: {
+      value: '',
+      error: false,
+    },
+    code: {
+      value: '',
+      error: false,
+    },
+    description: {
+      value: '',
+      error: false,
+    },
+    vendor: {
+      value: vendors[0],
+      error: false,
+    },
+    image: {
+      values: [],
+      open: false,
+    }
+  })
   
+  const formOnChange = (e) => {
+    if(form[e.target.name]){
+      form[e.target.name].value = e.target.value;
+      console.log(form)
+    }
+  }
   const handleSubmit = (e) => {
     console.log(e)
     setOpenSnack(true)
@@ -117,36 +150,37 @@ const Add = ({classes}) => {
             </Grid>
             <Grid item lg={12} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin}>
-                <TextField error={formErrors.name} helperText="Product Name" variant="outlined" id="name" label="Product Name"  />
+                <TextField onChange={formOnChange} error={form.name.error} helperText="Product Name" variant="outlined" name="name" label="Product Name"  />
               </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin}>
-                <TextField error={formErrors.stock} helperText="Stock" variant="outlined" id="stock" label="Stock"  />
+                <TextField error={form.stock.error} onChange={formOnChange} helperText="Stock" variant="outlined" name="stock" label="Stock"  />
               </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin} variant="outlined">
                 <InputLabel htmlFor="amount">Amount</InputLabel>
                 <OutlinedInput
-                  id="amount"
+                  name="amount"
                   startAdornment={<InputAdornment position="start">$</InputAdornment>}
                   labelWidth={60}
-                  error={formErrors.amount}
+                  onChange={formOnChange}
+                  error={form.amount.error}
                 />
-                <FormHelperText id="amount">Amount</FormHelperText>
+                <FormHelperText name="amount">Amount</FormHelperText>
               </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin} variant="outlined" >
-                <InputLabel id="category">Category</InputLabel>
+                <InputLabel name="category">Category</InputLabel>
                 <Select
-                  labelId="category"
-                  id="category"
-                  value={category}
+                  labelname="category"
+                  name="category"
+                  value={form.category.value}
                   label="category"
-                  error={formErrors.category}
-                  onChange={(event) => setCategory(event.target.value)}
+                  error={form.category.error}
+                  onChange={formOnChange}
                 >
                   <MenuItem value="">
                     <em>None</em>
@@ -157,75 +191,58 @@ const Add = ({classes}) => {
                     })
                   }
                 </Select>
-                <FormHelperText id="category">Select Category</FormHelperText>
+                <FormHelperText name="category">Select Category</FormHelperText>
               </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin} variant="outlined">
-                <InputLabel id="brand">Brands</InputLabel>
-                <Select
-                  labelId="brand"
-                  id="brand"
-                  value={brand}
-                  label="brands"
-                  error={formErrors.brand}
-                  onChange={(event) => setBrand(event.target.value)}
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  {
-                    brands.map((item, index) => {
-                      return(<MenuItem key={index} value={item.id}>{item.name}</MenuItem>)
-                    })
-                  }
-                </Select>
-                <FormHelperText id="brand">Select your brand</FormHelperText>
+                <Autocomplete
+                  name="brand"
+                  options={brands}
+                  onChange={(e, value) => {
+                    form.brand.value = value
+                    console.log("hey",form)
+                  }}
+                  getOptionLabel={(option) => option.name}
+                  defaultValue={form.brand.value}
+                  renderInput={(params) => <TextField {...params} label="Brand" variant="outlined" />}
+                />
+                <FormHelperText name="brand">Select your brand</FormHelperText>
               </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin} variant="outlined">
-                <TextField error={formErrors.model} helperText="Type the product model" variant="outlined" id="model" label="Model"  />
+                <TextField error={form.model.error} helperText="Type the product model" variant="outlined" name="model" label="Model"  />
               </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin} variant="outlined">
-                <TextField error={formErrors.code} helperText="Type product sku code" variant="outlined" id="code" label="Sku Code"  />
-              </FormControl>
-            </Grid>
-            <Grid item lg={12} xs={12} className={classes.formItem}>
-              <FormControl fullWidth className={classes.margin} variant="outlined">
-                  <InputLabel id="vendor">Vendors</InputLabel>
-                  <Select
-                    labelId="vendor"
-                    id="vendor"
-                    value={vendor}
-                    label="vendors"
-                    error={formErrors.vendor}
-                    onChange={(event) => setVendor(event.target.value)}
-                  >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  {
-                    vendors.map((item, index) => {
-                      return(<MenuItem key={index} value={item.id}>{item.name}</MenuItem>)
-                    })
-                  }
-                </Select>
-                <FormHelperText id="vendor">Select your vendor</FormHelperText>
+                <TextField error={form.code.error} helperText="Type product sku code" variant="outlined" name="code" label="Sku Code"  />
               </FormControl>
             </Grid>
             <Grid item lg={12} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin}>
-                <TextareaAutosize id="description" rowsMin={3} placeholder="Description" />
+                <Autocomplete
+                  name="vendor"
+                  options={vendors}
+                  onChange={formOnChange}
+                  getOptionLabel={(option) => option.name}
+                  defaultValue={form.vendor.value}
+                  renderInput={(params) => <TextField {...params} label="Vendor" variant="outlined" />}
+                />
+                <FormHelperText name="vendor">Select your vendor</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item lg={12} xs={12} className={classes.formItem}>
+              <FormControl fullWidth className={classes.margin}>
+                <TextareaAutosize name="description" rowsMin={3} placeholder="Description" />
               </FormControl>
             </Grid>
             <Grid item lg={12} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin}>
                 <Button className={`secondButton`} onClick={handleOpen.bind(this)}>Upload Image</Button>
                 <DropzoneDialog
-                  open={file.open}
+                  open={form.image.open}
                   onSave={handleSave.bind(this)}
                   acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
                   showPreviews={true}

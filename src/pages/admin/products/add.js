6573a1b0d019c8a48ddd028b3031b349e3f55d04
Luis Ho <evil_luis@hotmail.com>
2020-08-09 14:36:52 -0400

    work on form error

--- src/pages/admin/products/add.js
+++ src/pages/admin/products/add.js
@@ -21,7 +21,6 @@ import {
 } from '@material-ui/lab';
 import {DropzoneDialog} from 'material-ui-dropzone'
 import ColorPicker from 'material-ui-color-picker'
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 import Snackbar from '../../../components/common/Snackbar';
 import { CategorySample } from '../../../constants/admin/categories/CategorySample';
@@ -39,17 +38,17 @@ const styles = (theme) => ({
     width: '100%',
     textAlign: 'center',
   },
-  // margin: {
-  //   margin: theme.spacing(1),
-  //   [theme.breakpoints.down('sm')]: {
-  //     margin: 0,
-  //   },
-  // },
+  margin: {
+    margin: theme.spacing(1),
+    [theme.breakpoints.down('sm')]: {
+      margin: 0,
+    },
+  },
   formItems: {
-    width: '100%',
-    // [theme.breakpoints.down('sm')]: {
-    //   width: '100%',
-    // },
+    width: '50%',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
     margin: '20px auto 0px auto',
   },
   colorInput: {
@@ -64,11 +63,15 @@ const Add = ({classes}) => {
   const categories = CategorySample;
   const brands = BrandsSample;
   const vendors = VendorSample;
-  const [openSnack, setOpenSnack] = useState(false);
+  const [snack, setSnack] = useState({
+    severity: 'success',
+    open: false,
+    text: '',
+  });
   const [form, setForm] = useState({
     name: null,
     stock: null,
-    category: categories[0].id,
+    category: categories[0],
     amount: null,
     email: '',
     brand: brands[0],
@@ -81,17 +84,130 @@ const Add = ({classes}) => {
       open: false,
     }
   })
+
+  const [errors, setErrors] = useState({
+    name: {
+      error: false,
+      text: '',
+    },
+    stock: {
+      error: false,
+      text: '',
+    },
+    category: {
+      error: false,
+      text: '',
+    },
+    amount: {
+      error: false,
+      text: '',
+    },
+    email: {
+      error: false,
+      text: '',
+    },
+    brand: {
+      error: false,
+      text: '',
+    },
+    model: {
+      error: false,
+      text: '',
+    },
+    code: {
+      error: false,
+      text: '',
+    },
+    description: {
+      error: false,
+      text: '',
+    },
+    vendor: {
+      error: false,
+      text: '',
+    },
+    image: {
+      error: false,
+      text: '',
+    },
+  })
   
-  const formOnChange = ({ target: { name, value } }) => {
-    setForm({
-      ...form,
-      [name]: value === '' ? null : value,
-    });
+  const formOnChange = (e, edrop = null) => {
+    const { name, value } = edrop ? edrop : e.target;
+    if(name in form && validateForm(name, value)){
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    }
     console.log(form)
   }
+
   const handleSubmit = (e) => {
-    console.log(form)
-    setOpenSnack(true)
+    for(var i in form){
+      const error = validateForm(i, form[i]);
+      if(!error){
+        setSnack({
+          severity: 'error',
+          open: true,
+          text: errors[i].text,
+        })
+        return;
+      }
+    }
+    setSnack({
+      severity: 'success',
+      open: true,
+      text: 'Product Added',
+    })
+  }
+  const saveErrors = (key, err = false, str = '') => {
+    console.log(key, err, ' and ',str)
+    setErrors({
+      ...errors,
+      [key]: {
+        error: err,
+        text: str,
+      }
+    });
+    console.log(errors)
+  }
+  const validateForm = (name = null, value = null) => {
+    if(name){
+      saveErrors(name)
+      switch(name){
+        case "name":
+        case "stock":
+        case "model":
+        case "amount":
+        case "email":
+        case "description":
+        case "code": {
+          if(value && value.length > 0){
+            return true
+          }else{
+            console.log(name, 'anem')
+            saveErrors(name, true, `${name} is required`)
+            return false;
+          }
+          break;
+        }
+        case "category": 
+        case "brand": 
+        case "vendor": {
+          if(value && value.id){
+            return true
+          }
+          return false;
+          break;
+        }
+        default: {
+          return false;
+        }
+      }
+    }else{
+      console.log("check form ");
+    }
   }
   const handleClose = () => {
     setForm({
@@ -128,78 +244,83 @@ const Add = ({classes}) => {
   return (
     <AdminLayoutTemplate>
       <div className={classes.root}>
-        <ValidatorForm
-          onSubmit={handleSubmit}
-          onError={errors => console.log(errors)}
+        <form
+          // onSubmit={handleSubmit}
         >
           <Grid container spacing={2} className={classes.formItems}>
             <Grid item lg={12} xs={12}>
               <Typography align="center" variant="h4" component="h4">Add Product</Typography>
             </Grid>
-            <Grid item lg={12} xs={12}>
-              <TextValidator
-                  label="Name"
+            <Grid item lg={12} xs={12} className={classes.formItem}>
+              <FormControl fullWidth className={classes.margin} variant="outlined">
+                <TextField 
+                  error={errors.name.error}
+                  helperText={errors.name.text} 
+                  variant="outlined" 
+                  name="name" 
                   onChange={formOnChange}
-                  name="name"
-                  className={classes.formItems}
-                  value={form.name}
-                  validators={['required']}
-                  errorMessages={['this field is required']}
+                  label="Name" 
                 />
+              </FormControl>
             </Grid>
-            <Grid item lg={12} xs={12}>
-                <TextValidator
-                  label="Email"
-                  onChange={formOnChange}
-                  className={classes.formItems}
-                  name="email"
-                  value={form.email}
-                  validators={['required', 'isEmail']}
-                  errorMessages={['this field is required', 'email is not valid']}
+            <Grid item lg={12} xs={12} className={classes.formItem}>
+              <FormControl fullWidth className={classes.margin} variant="outlined">
+                <TextField 
+                  error={errors.email.error}
+                  helperText={errors.email.text} 
+                  variant="outlined" 
+                  name="email" 
+                  onChange={formOnChange} 
+                  label="Email" 
                 />
-            </Grid>
-            <Grid item lg={12} xs={12}>
-                <TextValidator
-                  label="Stock"
-                  onChange={formOnChange}
-                  className={classes.formItems}
-                  name="stock"
-                  value={form.stock}
-                  validators={['required']}
-                  errorMessages={['this field is required']}
-                />
+              </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
-              <TextValidator
-                  label="Amount"
+              <FormControl fullWidth className={classes.margin} variant="outlined">
+                <TextField 
+                  error={errors.stock.error}
+                  helperText={errors.stock.text} 
+                  variant="outlined" 
+                  name="stock" 
                   onChange={formOnChange}
-                  className={classes.formItems}
-                  name="amount"
-                  value={form.amount}
-                  validators={['required']}
-                  errorMessages={['this field is required']}
+                  label="Stock" 
                 />
+              </FormControl>
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
-                <TextValidator
-                  label="Amount"
+              <FormControl fullWidth className={classes.margin} variant="outlined">
+                <TextField 
+                  error={errors.amount.error}
+                  helperText={errors.amount.text} 
+                  variant="outlined" 
+                  name="amount" 
                   onChange={formOnChange}
-                  className={classes.formItems}
-                  name="amount"
-                  select
-                  value={form.amount}
-                  validators={['required']}
-                  errorMessages={['this field is required']}
+                  label="Amount" 
                 />
+              </FormControl>
             </Grid>
-           {/*  <Grid item lg={6} xs={12} className={classes.formItem}>
+            <Grid item lg={6} xs={12} className={classes.formItem}>
+              <FormControl fullWidth className={classes.margin} variant="outlined">
+                <Autocomplete
+                  name="category"
+                  options={categories}
+                  onChange={(e, value) => {
+                    formOnChange(null, { name: 'category', value: value})
+                  }}
+                  getOptionLabel={(option) => option.name}
+                  defaultValue={form.category}
+                  renderInput={(params) => <TextField {...params} label="Category" variant="outlined" />}
+                />
+                <FormHelperText name="category">Select your category</FormHelperText>
+              </FormControl>
+            </Grid>
+            <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin} variant="outlined">
                 <Autocomplete
                   name="brand"
                   options={brands}
                   onChange={(e, value) => {
-                    form.brand.value = value
-                    console.log("hey",form)
+                    formOnChange(null, { name: 'brand', value: value})
                   }}
                   getOptionLabel={(option) => option.name}
                   defaultValue={form.brand}
@@ -223,7 +344,9 @@ const Add = ({classes}) => {
                 <Autocomplete
                   name="vendor"
                   options={vendors}
-                  onChange={formOnChange}
+                  onChange={(e, value) => {
+                    formOnChange(null, { name: 'vendor', value: value})
+                  }}
                   getOptionLabel={(option) => option.name}
                   defaultValue={form.vendor}
                   renderInput={(params) => <TextField {...params} label="Vendor" variant="outlined" />}
@@ -248,7 +371,7 @@ const Add = ({classes}) => {
                   onClose={handleClose.bind(this)}
                 />
               </FormControl>
-            </Grid> */}
+            </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin}>
                 <Button className={`mainButton`}>Cancel</Button>
@@ -256,12 +379,13 @@ const Add = ({classes}) => {
             </Grid>
             <Grid item lg={6} xs={12} className={classes.formItem}>
               <FormControl fullWidth className={classes.margin}>
-                <Button type="submit" className={`mainButton`}>Add Product</Button>
+                {/* <Button type="submit" className={`mainButton`}>Add Product</Button> */}
+                <Button onClick={handleSubmit} className={`mainButton`}>Add Product</Button>
               </FormControl>
             </Grid>
           </Grid>
-        </ValidatorForm>
-        <Snackbar open={openSnack} onClose={() => setOpenSnack(false)} content="Product Added" />
+        </form>
+        <Snackbar open={snack.open} severity={snack.severity} onClose={() => setSnack({...snack, open: false})} content={snack.text} />
       </div>
     </AdminLayoutTemplate>
   );

a50ec406ad64b6c2fdff9b590c3aac55c97e2d4f
luishk807 <luishk807@hotmail.com>
2020-10-17 15:32:58 -0400

    move files for coming soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,8 @@
 import React, { Component } from 'react';
-import fetch from 'isomorphic-unfetch';
-import { 
-  Grid,
-} from '@material-ui/core';
-
-import LayoutTemplate from '../components/common/Layout/LayoutTemplate';
-import BigCarrousel from '../components/BigCarrousel';
-import ProductGallery from '../components/ProductGallery';
-import ProductCategoryIcons from '../components/ProductCategoryIcons'
-import NewArrival from '../components/NewArrival';
-import Maitenance from '../components/Maintenance';
 export default class extends Component {
   render () {
     return (
       <Maitenance/>
-      // <LayoutTemplate>
-      //   <Grid container className="main-section">
-      //     <Grid item>
-      //       <BigCarrousel image={'main-banner.jpg'} />
-      //       <ProductCategoryIcons />
-      //       <NewArrival title="Nuevas Llegadas" />
-      //       <ProductGallery galleryType='store-list' title="Tiendas" />
-      //       <ProductGallery galleryType='mini-banner'/>
-      //       <ProductGallery galleryType='store-feature' title="Nuevas Llegadas" />
-      //       <ProductGallery galleryType='brand-list' title="Top Brands" />
-      //     </Grid>
-      //   </Grid>
-      // </LayoutTemplate>
     )
   }
 }

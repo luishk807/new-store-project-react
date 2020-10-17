e3972330d80ed9bad63ada48b0cdb975a562a37f
luishk807 <luishk807@hotmail.com>
2020-10-17 18:18:51 -0400

    more update for maintenace mode

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -8,6 +8,17 @@ import {
 import Icons from '../components/common/Icons';
 import Typography from './common/Typography';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import PhoneIcon from '@material-ui/icons/Phone';
+import RoomIcon from '@material-ui/icons/Room';
+import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 const styles = (theme) => ({
   root: {
     width: '100%',
@@ -18,7 +29,24 @@ const styles = (theme) => ({
   },
   mainFont: {
     color: 'white',
-    fontFamily: 'Verdana'
+    fontFamily: 'Arial',
+    fontSize: '3em',
+    fontWeight: 'bold',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2em',
+    },
+  },
+  sectionlist: {
+    display: 'inline'
+  },
+  mainSub: {
+    color: 'white',
+    fontFamily: 'Arial',
+    fontSize: '1.3em',
+    fontWeight: 'bold',
+    display: 'inline-block',
+    padding: 10,
+    verticalAlign: 'middle'
   },
   container: {
     width: '100%',
@@ -27,15 +55,33 @@ const styles = (theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  icon: {
+    fill: 'white',
+    verticalAlign: 'center',
+  },
+  miniItems: {
+    verticalAlign: 'middle',
+    padding: 5
+  },
   item: {
-    width: '35%',
+    width: '40%',
     [theme.breakpoints.down('md')]: {
       width: '50%',
     },
     [theme.breakpoints.down('sm')]: {
       width: '80%',
     },
+  },
+  sectionRoot: {
+    position: 'relative',
+    display: 'flex'
+  },
+  flexContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   }
+  
 });
 
 const Maintenance = ({classes, data, title}) => {
@@ -44,7 +90,23 @@ const Maintenance = ({classes, data, title}) => {
       <Grid container className={classes.container}>
         <Grid item className={classes.item}>
           <Icons name="logoWhite"/>
-          <Typography className={classes.mainFont} align="center" variant="h3">Proximamente....</Typography>
+          <Typography className={classes.mainFont} align="center" variant="p">Proximamente....</Typography>
+        </Grid>
+        <Grid item lg={12} align="center">
+          <Grid container className={classes.flexContainer}>
+            <Grid item className={classes.miniItems}>
+              <PhoneIcon className={classes.icon}/>
+              <Typography className={classes.mainSub} align="center" variant="p">6770-2400</Typography>
+            </Grid>
+            <Grid item className={classes.miniItems}>
+              <RoomIcon className={classes.icon}/>
+              <Typography className={classes.mainSub} align="center" variant="p">Plaza El Dorado</Typography>
+            </Grid>
+            <Grid item className={classes.miniItems}>
+              <AlternateEmailIcon className={classes.icon}/>
+              <Typography className={classes.mainSub} align="center" variant="p">info@avenidaz.com</Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>

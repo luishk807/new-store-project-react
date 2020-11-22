52160129c47b05dd3eea43487e636ac19d40d4a4
luishk807 <luishk807@hotmail.com>
2020-11-22 18:56:52 -0500

    remove unnecessary api

--- src/api/banners.js
+++ src/api/banners.js
@@ -3,11 +3,3 @@ import Api from '../services/api';
 export const getMainSliders = async(data) => {
   return Api.get(`banners`, data);
 }
-
-export const getAnswers = async(data) => {
-  return Api.get(`productanswers`, data);
-}
-
-export const getAnswerById = async(data) => {
-  return Api.get(`productanswers`, data);
-}

a5c87b779bbb84d09dd985a115048a73d805e715
stevenova <dstevey@hotmail.com>
2021-04-10 23:12:09 -0500

    AV-79: Added floating Whatsapp button on the main layout template

---
+++ src/components/common/FabActionButtons.jsx
@@ -0,0 +1,43 @@
+import { Fab } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    bottomRight: {
+        marginRight: theme.spacing(1),
+        position: 'fixed',
+        bottom: '60px',
+        right: '30px',
+        zIndex: '180',
+    },
+    fabIcon: {
+        width: '40px',
+        height: '40px',
+    },
+    img: {
+        width: '40px'
+    }
+}));
+  
+const FabActionButtons = () => {
+    const classes = useStyles();
+
+    const openNewWindow = (url) => {
+        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
+        if (newWindow) newWindow.opener = null
+    }
+
+    const whatsappAction = () => {
+        openNewWindow('https://wa.me/50767702400');
+    }
+
+    return (
+        <div className={classes.bottomRight}>
+            <Fab size="small" color="primary" className={classes.fabIcon} aria-label="Cont&aacute;ctenos" onClick={whatsappAction}>
+                <img src="/images/social/logo-whatsapp.png" alt="Whatsapp" className={classes.img} />
+            </Fab>
+        </div>
+    )
+
+}
+
+export default FabActionButtons;

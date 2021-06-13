dfbb1487a1248eeeae726c18cf1898cf67ed0b14
luishk807 <luishk807@hotmail.com>
2021-06-13 14:55:27 -0400

    test app to function

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NextApp from 'next/app'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../../styles.scss'
 import { wrapper } from '../redux/store';
 import { appWithTranslation } from 'next-i18next'
@@ -15,20 +15,36 @@ const tagManagerArgs = {
 if (process.browser) {
   TagManager.initialize(tagManagerArgs);
 }
-class App extends NextApp {
-  componentDidMount() {
+// class App extends NextApp {
+//   componentDidMount() {
+//     const jssStyles = document.querySelector('#jss-server-side')
+//     if (jssStyles && jssStyles.parentNode)
+//       jssStyles.parentNode.removeChild(jssStyles)
+//   }
+//   render() {
+//     const { Component, pageProps } = this.props
+//     return (
+//       <HttpsRedirect>
+//         <Component {...pageProps} />
+//       </HttpsRedirect>
+//     )
+//   }
+// }
+
+const MyApp = ({ Component, pageProps, auth }) => {
+
+  useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
     if (jssStyles && jssStyles.parentNode)
       jssStyles.parentNode.removeChild(jssStyles)
-  }
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <HttpsRedirect>
-        <Component {...pageProps} />
-      </HttpsRedirect>
-    )
-  }
+  }, []);
+  
+  return (
+    // <Layout>
+      <Component {...pageProps} />
+      // </Layout>
+  )
 }
+// export default MyApp
 
-export default wrapper.withRedux(appWithTranslation(App))
+export default wrapper.withRedux(appWithTranslation(MyApp))

06bbc7c9a0519b0524b28ee5a5d2d8d58106ca23
luishk807 <luishk807@hotmail.com>
2021-06-13 16:45:40 -0400

    test

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -31,4 +31,52 @@ class App extends NextApp {
   }
 }
 
+App.getInitialProps = async (appContext) => {
+    // HTTPS redirect
+    if (process.env.NODE_ENV === 'production' && ctx.req && ctx.req.headers['x-forwarded-proto'] !== 'https') {
+      ctx.res.writeHead(302, {
+        Location: `https://${ctx.req.headers.host}${ctx.req.url}`,
+      });
+      ctx.res.end();
+      return;
+    }
+  
+    const { pathname } = ctx;
+    const route = getRouteByPath(pathname);
+  
+    // // private route check  access
+    // let isUserAuth = selectIsUserAuth(ctx.store.getState());
+    // if (process.env.NODE_ENV !== 'development' && route && route.protected) { // ToDo: Think about checking permissions
+    //   if (ctx.req) {
+    //     setSSRCookie(ctx.req.headers.cookie);
+    //   }
+    //   const user = await AuthService.getUser();
+    //   isUserAuth = !!user && user.status === 'loggedin';
+    //   if (!isUserAuth) {
+    //     if (!route.collection) {
+    //       if (ctx.res) {
+    //         ctx.res.writeHead(302, {
+    //           Location: '/',
+    //         });
+    //         ctx.res.end();
+    //       } else {
+    //         Router.push('/');
+    //       }
+    //     }
+    //     return;
+    //   } else {
+    //     setSSRAuthToken(user.authtoken);
+    //   }
+    // }
+  
+    return {
+      // authCheckDone: true,
+      // isUserAuth,
+      pageProps: {
+        // Call page-level getInitialProps
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
+      }
+    };
+}
+
 export default wrapper.withRedux(appWithTranslation(App))

f093b761b28d985884e68399167d42e5e40bda17
luishk807 <luishk807@hotmail.com>
2021-07-15 10:16:19 -0400

    more update

--- src/api/stGeorgePayment.js
+++ src/api/stGeorgePayment.js
@@ -8,14 +8,6 @@ export const uniqid = (prefix = "", random = false) => {
 };
 
 export const processPaymentCard = async(data) => {
-  // <input type="hidden" name="access_key" value="<REPLACE WITH ACCESS KEY>">
-  // <input type="hidden" name="profile_id" value="<REPLACE WITH PROFILE ID>">
-  // <input type="hidden" name="transaction_uuid" value="<?php echo uniqid() ?>">
-  // <input type="hidden" name="signed_field_names" value="access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,payment_method,bill_to_forename,bill_to_surname,bill_to_email,bill_to_phone,bill_to_address_line1,bill_to_address_city,bill_to_address_state,bill_to_address_country,bill_to_address_postal_code">
-  // <input type="hidden" name="unsigned_field_names" value="card_type,card_number,card_expiry_date">
-  // <input type="hidden" name="signed_date_time" value="<?php echo gmdate("Y-m-d\TH:i:s\Z"); ?>">
-  // <input type="hidden" name="locale" value="en">
-  
   data['access_key'] = process.env.STGEORGE_ACCESS_KEY;
   data['profile_id'] = process.env.STGEORGE_PROFILE_ID;
   data['transaction_uuid'] = uniqid();

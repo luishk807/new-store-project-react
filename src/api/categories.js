67046b565b5f3fcc70d988006c0485569400db9a
luishk807 <luishk807@hotmail.com>
2021-10-23 21:36:34 -0400

    allow user to set priority

--- src/api/categories.js
+++ src/api/categories.js
@@ -4,11 +4,23 @@ export const saveCategories = async(data) => {
   return Api.post(`categories`, data);
 }
 
+export const updateCategory = async(data) => {
+  return Api.save(`categories`, data);
+}
+
+export const setPriority = async(id) => {
+  return Api.save(`categories/${id}/priority`);
+}
+
 export const deleteCategeoryByUserId = async(data) => {
   const config = {
     'cotent-Type': 'application/x-www-form-urlencoded'
   }
   return Api.delete(`categories/${data.product}`, {}, config);
+}
+
+export const deleteCategoryById = async(id) => {
+  return Api.delete(`categories/${id}`);
 }
 
 export const getCategories = async(data) => {
